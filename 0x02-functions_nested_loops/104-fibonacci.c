b920800217dfb751cffb23ceb0de7f4887208d46
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-02-08 15:15:40 +0200

    first commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - main function
+*
+* Return: nothing
+*/
 int main(void)
 {
+int counter = 2;
 
-	return (0);
+float a = 1;
+float b = a + 1;
+float c = a + b;
+
+printf("%.0f, ", a);
+printf("%.0f, ", b);
+while (counter < 98)
+{
+counter++;
+printf("%.0f", c);
+a = b;
+b = c;
+c = a + b;
+if (counter < 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
 }
