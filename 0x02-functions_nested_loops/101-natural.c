591c83bba7f324d3f6efff2a92d684d14f08c328
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-02-08 14:50:42 +0200

    first commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int counter = 2;
-  long int a = 1;
-  long int b = a + 1;
-  long int c = a + b;
-​
-  printf("%ld, %ld, ", a, b);
-  while (counter < 50)
-  {
-  printf("%ld", c);
-  counter++;
-  a = b;
-  b = c;
-  c = a + b;
-  if (counter < 50)
-  {
-  printf(", ");
-  }
-  }
-  printf("\n");
-  return (0);
+
+	return (0);
 }
