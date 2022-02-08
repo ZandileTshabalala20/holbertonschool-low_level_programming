4fc1ade60d2ba55e750d443bff342f2d3eb77988
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-02-08 13:40:40 +0200

    first commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_sign - a function that prints the sign of a number
  * @n: number input
@@ -8,17 +8,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar ('0');
+		_putchar('0');
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
-		_putchar ('-');
+		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
