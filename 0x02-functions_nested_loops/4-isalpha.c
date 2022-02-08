331e3212fb5ebc38ed1606b01c3ef061578479b6
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-02-08 13:38:17 +0200

    first commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _isalpha - a function that checks for alphabetic character
  * @c: single letter input
@@ -6,7 +6,7 @@
  */
 int _isalpha(int c)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
