3997546074b170a70caefe22887d9f64093c3ce3
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-02-08 13:51:59 +0200

    first commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * _abs - a function that computes the absolute value of an integer
- * @ab: integer input
- * Return: absolute value of ab
- */
+*_abs - main function
+*@n: The number to be computed
+*
+*Return: zero value
+*
+*/
 int _abs(int n)
 {
-	if (n < 0)
-	{
-		return (-n);
-	}
-	else
-	{
-		return (n);
-	}
-	return (0);
+
+if (n > 0)
+return (n);
+else
+return (n * -1);
+
 }
