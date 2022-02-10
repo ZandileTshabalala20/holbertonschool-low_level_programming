78da8920b601b5a56a58ac6a708678c8a646d78e
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-02-10 13:09:58 +0200

    first commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9 expect for 2 and 4.
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
  *
  * Return: Always 0.
  */
 void print_most_numbers(void)
 {
-	int x;
+	int a;
 
-	for (x = 48; a <= 58; x++)
+	for (a = 48; a < 58; a++)
 	{
-		if (x != 50)
+		if (a != 50)
 		{
-			if (x != 52)
+			if (a != 52)
 			{
-			_putchar(x);
-			
+			_putchar(a);
+			}
 		}
 	}
 	_putchar('\n');
 }
-
