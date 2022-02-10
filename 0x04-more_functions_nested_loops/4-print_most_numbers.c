6f052a3343803620d1603c69bd61cbfa1cdaffd2
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-02-10 10:18:57 +0200

    first commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9 expect for 2 and 4.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; a <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(a);
+			
+		}
+	}
+	_putchar('\n');
+}
