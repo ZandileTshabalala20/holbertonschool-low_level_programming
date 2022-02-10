4e3710de2ae4d031fc013dbbb6d20cce8f84734f
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-02-10 09:06:46 +0200

    first commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - checks for a digit (o through 9)
+* @c: The number to be checked. 
+*
+* Return: 1 if the number is a digit, 0 otherwise.
+*/
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+		return (1);
+
+	else
+
+		return(0);
+}
